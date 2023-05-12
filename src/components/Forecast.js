96f8e5a3557db2c755d26d9308b752506f203f7d
khadiza9292 <khadizatul.qubra@chasacademy.se>
2023-05-12 09:34:59 +0200

    Last commit

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { RiSunFill, RiSunsetFill, RiTemperatureFill, RiWindFill , RiHumidityFill, RiEyeFill} from 'react-icons/ri';
-
-
+import { WiThermometer, WiSunrise, WiSunset, WiWindBeaufort3, WiHumidity, WiFog } from "react-icons/wi";
 
 const Forecast = ({ forecast }) => {
+  const getHumidity = (weatherData) => {
+    return weatherData.main.humidity;
+  };
+
+  const getSunrise = (weatherData) => {
+    const sunriseTimestamp = weatherData.sys?.sunrise;
+    const sunriseDate = new Date(sunriseTimestamp * 1000);
+    return sunriseDate.toLocaleTimeString();
+  };
+
+  const getSunset = (weatherData) => {
+    const sunsetTimestamp = weatherData.sys?.sunset;
+    const sunsetDate = new Date(sunsetTimestamp * 1000);
+    return sunsetDate.toLocaleTimeString();
+  };
+
+  const getWindSpeed = (weatherData) => {
+    return weatherData.wind.speed;
+  };
+
   const getForecastForNext5Days = () => {
     if (forecast.length === 0) {
       return null;
@@ -15,12 +33,32 @@ const Forecast = ({ forecast }) => {
       const date = new Date(item.dt_txt);
       const day = date.toLocaleDateString('en-US', { weekday: 'long' });
       const temperature = Math.round(item.main.temp);
+      const humidity = getHumidity(item);
+      const sunrise = getSunrise(item);
+      const sunset = getSunset(item);
+      const windSpeed = getWindSpeed(item);
 
       return (
         <div key={index} className="forecast-box">
-        <div className='forecast-item'>  
-          <div className="forecast-day">{day}</div>
-          <div className="forecast-temp">{temperature}°C</div></div>
+          <div className='forecast-item'>
+            <div className="forecast-day"> {day}</div>
+            <div className="forecast-temp">
+              <WiThermometer size={40}/>Temperature {temperature}°C
+            </div>
+            <div className="forecast-humidity">
+              <WiHumidity size={40}/> Humidity {humidity} %
+            </div>
+            <div className="forecast-sunrise">
+            <WiSunrise size={40}/> {sunrise ? `Sunrise: ${sunrise}` : ""}
+        
+            </div>
+            <div className="forecast-sunset">
+            <WiSunset size={40}/> {sunset ? `Sunset: ${sunset}` : ""}
+            </div>
+            <div className="forecast-wind">
+              <WiWindBeaufort3  size={40}/> Windspeed {windSpeed}
+            </div>
+          </div>
         </div>
       );
     });
@@ -30,7 +68,6 @@ const Forecast = ({ forecast }) => {
 
   return (
     <div>
-     
       {getForecastForNext5Days()}
     </div>
   );
